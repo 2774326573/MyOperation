68e08f37a09c278e09c27eb8cd105663a5d63eea
wuxingsong <2774326573@qq.com>
2025-06-06 00:10:52 +0800

    add modbus

---
+++ inc/config/SettingManager.h
@@ -0,0 +1,16 @@
+//
+// Created by jinxi on 25-6-5.
+//
+
+#ifndef SETTINGMANAGER_H
+#define SETTINGMANAGER_H
+
+
+
+class SettingManager {
+
+};
+
+
+
+#endif //SETTINGMANAGER_H
---
+++ src/config/SettingManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jinxi on 25-6-5.
+//
+
+#include "../../inc/config/SettingManager.h"
